9769902c1ff51b04812625f1bfb49deadac8dcfc
Jacinta <162335518+jhelm1217@users.noreply.github.com>
2024-03-27 09:18:44 -0400

    edited mainjs

--- main.js
+++ main.js
@@ -1,8 +1,5 @@
 const displayedImage = document.querySelector('.displayed-img');
-const thumbBar = document.querySelector('.thumb-bar');
 
-const btn = document.querySelector('button');
-const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
 const imageFileNames =['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
@@ -17,6 +14,7 @@ const imageAltText ={
 };
 
 /* Looping through images */
+const thumbBar = document.querySelector('.thumb-bar');
 
 imageFileNames.foreach(filename => {
     const newImage = document.createElement('img');
@@ -29,6 +27,13 @@ imageFileNames.foreach(filename => {
     thumbBar.appendChild(newImage);
 })
 /* Wiring up the Darken/Lighten button */
+
+const btn = document.querySelector('button');
+const overlay = document.querySelector('.overlay');
+
+btn.setAttribute('data-dark', 'button-class');
+btn.textContent = isDarkened;
+
 let isDarkened = flase;
 darkenBtn.addEventListner('click', () => {
     isDarkened = !isDarkened;
