43858da5687c5bafe51001a9f0c6b1505995e742
Jacinta <162335518+jhelm1217@users.noreply.github.com>
2024-03-27 18:22:17 -0400

    added functions to js

--- main.js
+++ main.js
@@ -1,45 +1,37 @@
 const displayedImage = document.querySelector('.displayed-img');
-
-
-/* Declaring the array of image filenames */
-const imageFileNames =['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
-
-/* Declaring the alternative text for each image file */
-const imageAltText ={
-    'pic1.jpg' : 'Image 1 Alt Text',
-    'pic2.jpg' : 'Image 2 Alt Text',
-    'pic3.jpg' : 'Image 3 Alt Text',
-    'pic4.jpg' : 'Image 4 Alt Text',
-    'pic5.jpg' : 'Image 5 Alt Text'
-};
-
-/* Looping through images */
 const thumbBar = document.querySelector('.thumb-bar');
-
-imageFileNames.foreach(filename => {
-    const newImage = document.createElement('img');
-    newImage.setAttribute('src', imageFileNames);
-    newImage.setAttribute('alt', imageAltText);
-    img.addEventListener('click', () => {
-        displayedImg.src = imageFileNames;
-        displayedImg.alt = imageAltText;
-    })
-    thumbBar.appendChild(newImage);
-})
-/* Wiring up the Darken/Lighten button */
-
-const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
-btn.setAttribute('data-dark', 'button-class');
-btn.textContent = isDarkened;
+/* Declaring the array of image filenames */
+const images =['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
 
-let isDarkened = flase;
-darkenBtn.addEventListner('click', () => {
-    isDarkened = !isDarkened;
-    if (isDarkened) {
-        displayedImg.classList.add('darken'); /*this will apply the dark feature*/
-    } else {
-        displayedImg.classList.remove('darken'); /* this will remove the dark effect*/
+/* Declaring the alternative text for each image file */
+const altTexts = ['close up of eye', 'beige waves', 'purple flowers', 'hieroglyphics', 'butterfly'];
+
+/* Looping through images */
+images.forEach((filename, index) => { 
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', 'images/' + images[index] + '.png');
+    newImage.setAttribute('alt', altTexts[index]);
+    newImage.addEventListener('click', () => {
+        displayedImage.setAttribute('src', 'images/' + filename);
+        displayedImage.setAttribute('alt', altTexts[index]);
+    });
+    thumbBar.appendChild(newImage);
+});
+
+/* Wiring up the Darken/Lighten button */
+
+const btn= document.querySelector('button');
+
+btn.addEventListener('click', function() {
+    if (btn.classList.contains('dark')) {
+        btn.classList.remove('dark');
+        btn.textContent = 'Darken';
+        overlay.style.backgroundColor = "rgba(0, 0, 0, 0)";
+    }   else {
+        btn.classList.add('dark');
+        btn.textContent = 'Lighten';
+        overlay.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
     }
 });
