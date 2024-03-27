8ae1f9942bc1c245058009874b2528685b013f34
Jacinta <162335518+jhelm1217@users.noreply.github.com>
2024-03-27 07:30:28 -0400

    added to js

--- main.js
+++ main.js
@@ -5,8 +5,16 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const imageFileNames =['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
 
 /* Declaring the alternative text for each image file */
+const imageAltText ={
+    'pic1.jpg' : 'Image 1 Alt Text',
+    'pic2.jpg' : 'Image 2 Alt Text',
+    'pic3.jpg' : 'Image 3 Alt Text',
+    'pic4.jpg' : 'Image 4 Alt Text',
+    'pic5.jpg' : 'Image 5 Alt Text'
+};
 
 /* Looping through images */
 
